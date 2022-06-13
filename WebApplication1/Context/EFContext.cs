2a3e9ac74da23e43d6ff188aedc283bb9be18806
Vinícius César Neves de Brito <v.cesar@academico.ifrn.edu.br>
2022-06-13 08:23:57 -0300

    banco de dados index create

--- WebApplication1/Context/EFContext.cs
+++ WebApplication1/Context/EFContext.cs
@@ -9,7 +9,7 @@ namespace WebApplication1.Context
 {
     public class EFContext : DbContext
     {
-        public EFContext() : base("Asp_Nt_MVC_CS") { }
+        public EFContext() : base("Asp_Net_MVC_CS") { }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
     }
